54ddc240335db372c8ac061206896eddce3392c7
artemvolkovplay@gmail.com <artemvolkovplay@gmail.com>
2023-10-23 15:31:27 +0300

    Добавляет изменения

--- webpack.config.ts
+++ webpack.config.ts
@@ -9,19 +9,19 @@ export default (env: BuildEnv) => {
     entry: path.resolve(__dirname, 'src', 'index.tsx'),
     build: path.resolve(__dirname, 'build'),
     html: path.resolve(__dirname, 'public', 'index.html'),
-    src: path.resolve(__dirname, 'src')
+    src: path.resolve(__dirname, 'src'),
   }
 
   const mode = env.mode || 'development'
   const isDev = mode === 'development'
   const PORT = env.port || 3000
 
-  // конфигурация webpack для сборки приложения в зависимости от переменных окружения 
+  // конфигурация webpack для сборки приложения в зависимости от переменных окружения
   const config: webpack.Configuration = buildWebpackConfig({
     mode: mode,
     path: paths,
     isDev: isDev,
-    port: PORT
+    port: PORT,
   })
 
   return config
